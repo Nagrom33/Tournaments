df296b7a2c62282e2a8231b2d02f3e8f407199c8
Gurdtl <gerroy@gerroy.nl>
2022-01-29 17:05:56 +0100

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
+// import NodeGlobalsPolyfillPlugin from 'rollup-plugin-node-polyfills';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -8,10 +9,13 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    // NodeGlobalsPolyfillPlugin({
+    //   buffer: true,
+    // })
   ],
   define: {
     process: {
-      "global": {},
+      global: 'globalThis',
       env: {},
       browser: true,
     }
